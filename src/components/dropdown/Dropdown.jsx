2e882e984482dcad4f8e2fc0464ec723828ac8c8
govind.govind <govind.govind@cvent.com>
2023-11-14 18:58:40 +0530

    fixing next errors

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -6,7 +6,7 @@ function Dropdown({content}) {
     <div>
       <div className={styles.dropdown}>
         <button className={styles.dropbtn}>{content.title}</button>
-        <svg width="4.7" height="8" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="zZ3yfL"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#fff" class="_2gTTdy"></path></svg>
+        <svg width="4.7" height="8" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" className="zZ3yfL"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#fff" className="_2gTTdy"></path></svg>
         <div className={styles.dropdown_content}>
           {/* <a href="#">My profile</a>
           <a href="#">orders</a>
